130fc220da3dbda7ee4c03aff099276f1ed28049
Paweł Kochanek <flagoon@o2.pl>
2018-02-06 21:46:24 +0100

    Added normalize and some prefixer shit.

--- image-presenter/src/index.js
+++ image-presenter/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css";
+import App from "./Components/App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
